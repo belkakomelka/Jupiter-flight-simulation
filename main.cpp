948938ba8a49156891d474c53efb965a9f030adf
Kamilya <Kamilya0902@yandex.ru>
2024-04-14 23:07:34 +0300

    forget about iterations in main

--- main.cpp
+++ main.cpp
@@ -26,7 +26,7 @@ int main() {
     std::cout << std::endl;
     std::vector<double> residuals;
     // todo вывод начальных
-    newtonModyfied(&getResidual, constants::dimension, x, residuals, constants::iterations_rk);
+    newtonModyfied(&getResidual, constants::dimension, x, residuals, constants::iterations);
     // todo вывод результатов
     return 0;
 }
